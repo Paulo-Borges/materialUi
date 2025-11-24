7fa142cc659d6fc1899e00e3d0e3ec56678e7780
Borges <borgespaulo72@yahoo.com.br>
2025-11-24 18:30:15 -0300

    feat: Adicionado tema claro e ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { ThemeProvider } from "@mui/material";
+import { LightTheme } from "./shared/themes/Light.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <ThemeProvider theme={LightTheme}>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </ThemeProvider>
 );
